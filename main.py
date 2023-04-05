ff2ad2e241f301d19d12526895c444006d3223c4
Marks <meshkis.mark@gmail.com>
2023-04-06 00:35:44 +0300

    Spēlēs iestatījumi un komentāri

--- main.py
+++ main.py
@@ -1,17 +1,35 @@
-import os
-import random
-import math
-import pygame
-from os import listdir
-from os.path import isfile, join
-pygame.init()
+import os  # importējam os moduli, kas palīdzēs strādāt ar failiem un direktorijām
+import random  # importējam random moduli, lai izmantotu gadījuma skaitļus
+import math  # importējam math moduli, lai izmantotu matemātiskas funkcijas
+import pygame  # importējam pygame moduli, kas palīdzēs radīt spēles
+from os import listdir  # no os importējam listdir funkciju, kas atgriež sarakstu ar failiem direktorijā
+from os.path import isfile, join  # no os.path importējam isfile un join funkcijas
 
-pygame.display.set_caption("2D Platformer")
+pygame.init()  # inicializē pygame moduli
 
-BG_COLOR = (255, 255, 255)
-WIDTH, HEIGHT = 1000, 800
-FPS=60
-PLAYER_VEL = 5
+pygame.display.set_caption("2D Platformer") # nosaka loga virsrakstu
 
-window = pygame.display.set_mode((WIDTH, HEIGHT))
+BG_COLOR = (255, 255, 255) # nosaka fona krāsu
+WIDTH, HEIGHT = 1000, 800 # nosaka ekrāna izmēru
+FPS = 60 # nosaka bilžu attēlošanas ātrumu
+PLAYER_VEL = 5 # nosaka spēlētāja ātrumu
 
+window = pygame.display.set_mode((WIDTH, HEIGHT))  # izveido spēles logu ar definētu izmēru
+
+def main(window):  #galvenais logs, atbild par spēlēs uzsākšanu (iestatījumi)
+    clock = pygame.time.Clock()
+
+    run = True
+    while run:
+        clock.tick(FPS)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                break
+
+    pygame.quit()
+    quit()
+
+    if __name__ == "__main__":
+        main(window)
