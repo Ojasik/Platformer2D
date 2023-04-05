89b332e0a02cbdebb90861a085175c6a1079d8ce
Marks <meshkis.mark@gmail.com>
2023-04-06 00:43:34 +0300

    Background pievienošana

--- main.py
+++ main.py
@@ -16,6 +16,33 @@ PLAYER_VEL = 5 # nosaka spēlētāja ātrumu
 
 window = pygame.display.set_mode((WIDTH, HEIGHT))  # izveido spēles logu ar definētu izmēru
 
+
+def get_background(name):  # funkcija, lai iegūtu fona elementus un attēlu
+    image = pygame.image.load(join("assets", "Background", name))
+    _, _, width, height = image.get_rect()
+    tiles = []
+
+    for i in range(WIDTH // width + 1):
+        for j in range(HEIGHT // height + 1):
+            pos = (i * width, j * height)
+            tiles.append(pos)
+
+    return tiles, image
+
+
+def draw(window, background, bg_image, player, objects, offset_x):  # funkcija objektu zīmēšanai uz ekrāna
+    for tile in background:
+        window.blit(bg_image, tile)
+
+    for obj in objects:
+        obj.draw(window, offset_x)
+
+    player.draw(window, offset_x)
+
+    pygame.display.update()
+
+
+
 def main(window):  #galvenais logs, atbild par spēlēs uzsākšanu (iestatījumi)
     clock = pygame.time.Clock()
 
